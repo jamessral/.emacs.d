08a19ec3b46fe17b5586a0426f8bbae4947d0bfa
James Sral <james.sral@gmail.com>
2018-02-26 09:36:30 -0500

    Reintroduce gruvbox and rjsx mode

---
+++ elpa/flycheck-crystal-20171124.740/flycheck-crystal-autoloads.el
@@ -0,0 +1,16 @@
+;;; flycheck-crystal-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("flycheck-crystal.el") (23182 65157 121720
+;;;;;;  892000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-crystal-autoloads.el ends here
---
+++ elpa/flycheck-crystal-20171124.740/flycheck-crystal-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-crystal" "20171124.740" "Add support for Crystal to Flycheck" '((flycheck "30")) :commit "0fe6815201bebe4c5ff6857bd541d95b05132b10" :url "https://github.com/crystal-lang-tools/emacs-crystal-mode" :keywords '("tools" "crystal"))
