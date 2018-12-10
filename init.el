ebc7d29e45c27dd2c2ca9cf0d3c09edeacfa07e0
James A. Sral <jamessral@gmail.com>
2018-12-10 09:47:18 -0500

    Add wrap region global mode

--- init.el
+++ init.el
@@ -353,6 +353,12 @@
              :ensure t)
 (global-set-key (kbd "C-=") 'er/expand-region)
 
+
+(use-package wrap-region
+  :ensure t
+  :init
+  (wrap-region-global-mode))
+
 ;; Case sensitive company mode
 (setq company-dabbrev-downcase nil)
 
@@ -1174,11 +1180,15 @@
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(custom-safe-themes
    (quote
-    ("446cc97923e30dec43f10573ac085e384975d8a0c55159464ea6ef001f4a16ba" default))))
+    ("446cc97923e30dec43f10573ac085e384975d8a0c55159464ea6ef001f4a16ba" default)))
+ '(electric-pair-mode t)
+ '(package-selected-packages
+   (quote
+    (wrap-region yafolding xref-js2 writeroom-mode window-numbering web-mode w3m vue-mode use-package tide sublime-themes spotify spacemacs-theme smex smartparens scss-mode ruby-test-mode ruby-end robe rjsx-mode rinari rake racket-mode paredit pacmacs org-bullets olivetti oceanic-theme neotree navigate multi-term mocha magit lush-theme lsp-rust lsp-javascript-typescript linum-relative key-chord json-mode js2-refactor irony hemisu-theme helm-projectile helm-ag haxe-mode haxe-imports gruvbox-theme go-autocomplete github-theme git-gutter-fringe+ geiser flycheck-rust flycheck-elixir flatui-theme flatui-dark-theme fiplr expand-region exec-path-from-shell evil-surround evil-leader evil-escape evil-commentary evil-collection enh-ruby-mode elpy dracula-theme dashboard d-mode counsel-projectile company-racer company-go color-theme-solarized color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized clojure-mode-extra-font-locking cider better-defaults beacon base16-theme avy all-the-icons alchemist ag add-node-modules-path))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ )
