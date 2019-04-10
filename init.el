0c0a890627da29c4ff835b572f0cab15e56d18eb
James Sral <jamessral@gmail.com>
2019-04-10 16:38:48 -0400

    Restore evil

--- init.el
+++ init.el
@@ -113,6 +113,125 @@
 (setq evil-want-keybinding nil)
 (setq evil-want-integration nil)
 
+;;; Evil
+(use-package evil-leader
+  :ensure t
+  :init
+  :config
+  (evil-leader/set-leader "<SPC>")
+  (evil-leader/set-key
+   "b" 'switch-to-buffer
+   "<SPC>" 'counsel-M-x
+   "n" 'neotree-toggle
+   "m" 'neotree-find
+   "<RET>" 'save-buffer
+   "p" 'projectile-find-file
+   "S" 'magit-status
+   "j" 'avy-goto-line
+   "v" 'evil-window-vsplit
+   "s" 'evil-window-split
+   "/" 'evil-ex-nohighlight
+   "w" 'avy-goto-char-2)
+  (global-evil-leader-mode))
+
+(use-package evil-escape
+  :ensure t
+  :commands evil-escape-mode
+  :init
+  (setq evil-escape-excluded-states '(normal visual multiedit emacs motion)
+        evil-escape-excluded-major-modes '(neotree-mode)
+        evil-escape-key-sequence "jk"
+        evil-escape-delay 0.25)
+  (add-hook 'after-init-hook #'evil-escape-mode)
+  :config
+  ;; no `evil-escape' in minibuffer
+  (cl-pushnew #'minibufferp evil-escape-inhibit-functions :test #'eq)
+
+  (define-key evil-insert-state-map  (kbd "C-g") #'evil-escape)
+  (define-key evil-replace-state-map (kbd "C-g") #'evil-escape)
+  (define-key evil-visual-state-map  (kbd "C-g") #'evil-escape)
+  (define-key evil-operator-state-map (kbd "C-g") #'evil-escape))
+
+(use-package evil
+  :ensure t
+  :init
+  :config
+  (evil-mode 1)
+  (key-chord-mode 1)
+  (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
+  (key-chord-define evil-visual-state-map "jk" 'evil-normal-state)
+  (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+  (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+  (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+  (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+  (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+  ; (define-key evil-normal-state-map (kbd "RET") 'save-buffer)
+  (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+  (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+  (define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
+  (define-key evil-visual-state-map (kbd "C-d") 'evil-delete-char)
+  (define-key evil-insert-state-map (kbd "C-d") 'evil-delete-char)
+
+                                        ; Shamelessly stolen from Amir Rajan
+  (global-set-key [escape] 'evil-exit-emacs-state)
+
+  (defun evil-send-string-to-terminal (string)
+    (unless (display-graphic-p) (send-string-to-terminal string)))
+
+  (defun evil-terminal-cursor-change ()
+    (when (string= (getenv "TERM_PROGRAM") "iTerm.app")
+      (add-hook 'evil-insert-state-entry-hook (lambda () (evil-send-string-to-terminal "\e]50;CursorShape=1\x7")))
+      (add-hook 'evil-insert-state-exit-hook  (lambda () (evil-send-string-to-terminal "\e]50;CursorShape=0\x7"))))
+    (when (and (getenv "TMUX")  (string= (getenv "TERM_PROGRAM") "iTerm.app"))
+      (add-hook 'evil-insert-state-entry-hook (lambda () (evil-send-string-to-terminal "\ePtmux;\e\e]50;CursorShape=1\x7\e\\")))
+      (add-hook 'evil-insert-state-exit-hook  (lambda () (evil-send-string-to-terminal "\ePtmux;\e\e]50;CursorShape=0\x7\e\\")))))
+
+  (evil-terminal-cursor-change)
+  )
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+(use-package evil-surround
+  :ensure t
+  :config
+  (global-evil-surround-mode 1))
+
+(use-package evil-commentary
+  :ensure t
+  :config
+  (evil-commentary-mode))
+
+;; Evil disabled by default
+(evil-mode -1)
+(evil-escape-mode -1)
+(evil-leader-mode -1)
+
+(defun enable-evil ()
+  (interactive)
+  (set-relative-lines)
+  (evil-mode 1)
+  (evil-escape-mode 1)
+  (evil-leader-mode 1))
+
+(defun disable-evil ()
+  (interactive)
+  (set-relative-lines)
+  (evil-mode -1)
+  (evil-escape-mode -1)
+  (evil-leader-mode -1))
+
+(global-set-key (kbd "C-c e o") 'enable-evil)
+(global-set-key (kbd "C-c e f") 'disable-evil)
+(global-set-key (kbd "C-c e l") 'toggle-relative-lines)
+
 (defun set-relative-lines ()
   (interactive)
   (setq-default display-line-numbers 'relative))
