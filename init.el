54944bd5ca4accc6be865ecf1684dc808090c713
James Sral <jamessral@gmail.com>
2019-10-13 15:54:22 -0400

    Add modes for racket and clojure because <3 parens

--- init.el
+++ init.el
@@ -427,6 +427,8 @@
   (yafolding-mode 1))
 
 ;; Rainbow Mode hooks
+(use-package rainbow-delimiters
+  :ensure t)
 (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'racket-mode-hook #'rainbow-delimiters-mode)
 
@@ -992,6 +994,18 @@
 ;;; End Elixir
 
 
+;;; Clojure
+(use-package cider
+  :ensure t)
+;;; End Clojure
+
+
+;;; Racket
+(use-package racket-mode
+  :ensure t)
+;;; End Racket
+
+
 ;;; OrgMode
 (use-package org
   :ensure t
@@ -1320,7 +1334,7 @@
  '(org-agenda-files (quote ("~/org/work.org")))
  '(package-selected-packages
    (quote
-    (spacemacs-theme lsp-solargraph lsp-ruby-mode lsp-ruby lsp-elixir lsp-haskell company-haskell mu4e elm-mode haskell-mode company-lua luarocks flymake-lua lua-mode lsp-treemacs pyenv-mode-auto pyenv-auto pyenv-auto-mode lsp-imenu dap-mode company-jedi spinner lsp-mode undo-tree zenburn zenburn-theme forge omnisharp org htmlize ox-reveal goto-last-change spaceline doom-modeline org-mode evil-tab fish-mode yaml-mode yafolding xref-js2 writeroom-mode wrap-region window-numbering which-key web-mode vue-mode use-package tide sublime-themes spotify smex smartparens scss-mode ruby-test-mode ruby-end rubocopfmt rspec-mode robe rjsx-mode rinari restart-emacs rbenv racket-mode projectile-rails prettier-js paredit org-bullets olivetti neotree multi-term mocha magit lush-theme lsp-rust lsp-javascript-typescript linum-relative key-chord json-mode jedi irony indium helm-rg helm-projectile helm-ag haxe-mode haxe-imports haml-mode gruvbox-theme graphql-mode go-autocomplete git-gutter-fringe+ geiser flycheck-rust flycheck-elixir flycheck-crystal fiplr expand-region exec-path-from-shell evil-surround evil-leader evil-escape evil-commentary evil-collection enh-ruby-mode emmet-mode elpy dashboard d-mode crystal-mode counsel company-racer company-lsp company-go color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized clojure-mode-extra-font-locking cider better-defaults beacon base16-theme avy all-the-icons alchemist ag add-node-modules-path)))
+    (rainbow-delimiters spacemacs-theme lsp-solargraph lsp-ruby-mode lsp-ruby lsp-elixir lsp-haskell company-haskell mu4e elm-mode haskell-mode company-lua luarocks flymake-lua lua-mode lsp-treemacs pyenv-mode-auto pyenv-auto pyenv-auto-mode lsp-imenu dap-mode company-jedi spinner lsp-mode undo-tree zenburn zenburn-theme forge omnisharp org htmlize ox-reveal goto-last-change spaceline doom-modeline org-mode evil-tab fish-mode yaml-mode yafolding xref-js2 writeroom-mode wrap-region window-numbering which-key web-mode vue-mode use-package tide sublime-themes spotify smex smartparens scss-mode ruby-test-mode ruby-end rubocopfmt rspec-mode robe rjsx-mode rinari restart-emacs rbenv racket-mode projectile-rails prettier-js paredit org-bullets olivetti neotree multi-term mocha magit lush-theme lsp-rust lsp-javascript-typescript linum-relative key-chord json-mode jedi irony indium helm-rg helm-projectile helm-ag haxe-mode haxe-imports haml-mode gruvbox-theme graphql-mode go-autocomplete git-gutter-fringe+ geiser flycheck-rust flycheck-elixir flycheck-crystal fiplr expand-region exec-path-from-shell evil-surround evil-leader evil-escape evil-commentary evil-collection enh-ruby-mode emmet-mode elpy dashboard d-mode crystal-mode counsel company-racer company-lsp company-go color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized clojure-mode-extra-font-locking cider better-defaults beacon base16-theme avy all-the-icons alchemist ag add-node-modules-path)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
