efc62e32fb40cded79073e910840ac68d3b0efda
James A. Sral <jamessral@gmail.com>
2019-10-16 11:39:32 -0400

    Add better join-lines (concat-lines)

--- init.el
+++ init.el
@@ -87,6 +87,12 @@
 (setq mac-command-modifier 'meta)
 
 (global-set-key (kbd "C-h") 'help)
+;; join line to next line
+(defun concat-lines ()
+  (interactive)
+  (join-line -1))
+
+(global-set-key (kbd "C-j") 'concat-lines)
 
 (use-package writeroom-mode
   :ensure t
@@ -369,6 +375,7 @@
   :config
   ;; Use Paredit to allow slurping
   (global-set-key (kbd "C-)") 'paredit-forward-slurp-sexp)
+  (define-key paredit-mode-map (kbd "C-j") 'concat-lines)
   (add-hook 'emacs-lisp-mode-hook #'paredit-mode))
 
 ;; Expand Region
@@ -929,6 +936,8 @@
   :ensure t
   :commands lsp
   :init
+  (add-hook 'lsp-mode #'lsp-lens-mode)
+  (add-hook 'lsp-mode #'lsp-ui)
   (add-hook 'js-mode-hook #'lsp)
   (add-hook 'typescript-mode-hook #'lsp) ;; for typescript support
   (add-hook 'js3-mode-hook #'lsp) ;; for js3-mode support
@@ -941,10 +950,10 @@
   ;; (add-hook 'python-mode-hook #'lsp)
   )
 
-;; (use-package lsp-ui
-  ;; :defer t
-  ;; :ensure t
-  ;; :commands lsp-ui-mode)
+(use-package lsp-ui
+  :defer t
+  :ensure t
+  :commands lsp-ui-mode)
 
 (use-package company-lsp
   :ensure t
@@ -1332,7 +1341,7 @@
  '(org-agenda-files (quote ("~/org/work.org")))
  '(package-selected-packages
    (quote
-    (racket-mode cider rainbow-delimiters zenburn-theme yaml-mode yafolding xref-js2 writeroom-mode wrap-region window-numbering which-key web-mode vue-mode use-package undo-tree tide sublime-themes spotify spacemacs-theme smex smartparens scss-mode rust-mode ruby-test-mode ruby-end rubocopfmt rspec-mode robe rjsx-mode restart-emacs rbenv pyenv-mode-auto projectile-rails prettier-js poly-R paredit ox-reveal org-bullets omnisharp olivetti neotree naysayer-theme multi-term mocha lush-theme luarocks lsp-vue lsp-treemacs lsp-ruby lsp-haskell lsp-elixir linum-relative key-chord json-mode jedi irony indium htmlize helm-rg helm-projectile helm-ag haml-mode gruvbox-theme graphql-mode goto-last-change go-autocomplete git-gutter-fringe+ general forge flymake-lua flycheck-rust flycheck-haskell flycheck-elm flycheck-elixir fish-mode fiplr expand-region exec-path-from-shell ess enh-ruby-mode emmet-mode elpy elm-mode ein dap-mode d-mode counsel company-racer company-lua company-lsp company-jedi company-go color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized better-defaults beacon base16-theme all-the-icons alchemist ag add-node-modules-path)))
+    (lsp-ui racket-mode cider rainbow-delimiters zenburn-theme yaml-mode yafolding xref-js2 writeroom-mode wrap-region window-numbering which-key web-mode vue-mode use-package undo-tree tide sublime-themes spotify spacemacs-theme smex smartparens scss-mode rust-mode ruby-test-mode ruby-end rubocopfmt rspec-mode robe rjsx-mode restart-emacs rbenv pyenv-mode-auto projectile-rails prettier-js poly-R paredit ox-reveal org-bullets omnisharp olivetti neotree naysayer-theme multi-term mocha lush-theme luarocks lsp-vue lsp-treemacs lsp-ruby lsp-haskell lsp-elixir linum-relative key-chord json-mode jedi irony indium htmlize helm-rg helm-projectile helm-ag haml-mode gruvbox-theme graphql-mode goto-last-change go-autocomplete git-gutter-fringe+ general forge flymake-lua flycheck-rust flycheck-haskell flycheck-elm flycheck-elixir fish-mode fiplr expand-region exec-path-from-shell ess enh-ruby-mode emmet-mode elpy elm-mode ein dap-mode d-mode counsel company-racer company-lua company-lsp company-jedi company-go color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized better-defaults beacon base16-theme all-the-icons alchemist ag add-node-modules-path)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
