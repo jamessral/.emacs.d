aa8b3352ddbb6febc74673ac6ce1ad94cddafeb4
James Sral <jamessral@gmail.com>
2019-11-13 18:22:05 -0500

    Fix xterm-color init

--- init.el
+++ init.el
@@ -494,9 +494,9 @@
   (add-hook 'eshell-before-prompt-hook
             (lambda ()
               (setq xterm-color-preserve-properties t)))
-  (add-to-list 'eshell-preoutput-filter-functions 'xterm-color-filter)
-  (setq eshell-output-filter-functions
-        (remove 'eshell-handle-ansi-color eshell-output-filter-functions))
+  ;; (add-to-list 'eshell-preoutput-filter-functions 'xterm-color-filter)
+  ;; (setq eshell-output-filter-functions
+        ;; (remove 'eshell-handle-ansi-color eshell-output-filter-functions))
   (setenv "TERM" "xterm-256color"))
 
 (defun colorize-compilation-buffer ()
