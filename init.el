e44fc18b36cb1ceab94041a220910db67239906a
James Sral <jamessral@gmail.com>
2020-03-13 23:17:01 -0400

    Basically steal colors from LDG

--- init.el
+++ init.el
@@ -1302,7 +1302,7 @@ Version 2016-01-12"
 
 (defun load-very-dark ()
   (interactive)
-  (load-theme 'sanityinc-tomorrow-bright t))
+  (load-theme 'alect-black-alt t))
 
 (defun load-light ()
   (interactive)
@@ -1312,7 +1312,7 @@ Version 2016-01-12"
   (interactive)
   (load-theme 'sanityinc-tomorrow-blue t))
 
-(load-dark)
+(load-very-dark)
 
 (global-set-key (kbd "C-c u l") 'load-light)
 (global-set-key (kbd "C-c u d") 'load-dark)
@@ -1419,31 +1419,23 @@ Version 2016-01-12"
   :ensure t)
 ;;; End Godot
 
+
 ;;; C/C++
-(add-hook 'c-mode-hook
-          (setq c-default-style "linux"
-                tab-width 4
-                indent-tabs-mode t
-                c-basic-offset 4)
-          (flycheck-mode -1)
-          ;; Indent case statements
-          (c-set-offset 'case-label '+))
-
-(add-hook 'c++-mode-hook
-          (setq c-default-style "linux"
-                tab-width 4
-                indent-tabs-mode t
-                c-basic-offset 4)
-          (flycheck-mode -1)
-          ;; Indent case statements
-          (c-set-offset 'case-label '+))
-
+(setq-default c-default-style "linux"
+      tab-width 4
+      indent-tabs-mode t
+      c-basic-offset 4)
 (setq-default c-basic-offset 4)
+
+(add-hook 'c-mode-hook 'irony-mode)
+
+(add-hook 'c++-mode-hook 'irony-mode)
+
 
 (use-package irony
   :ensure t
   :config
-  (add-hook 'irony-mode-hook '(lambda () (flycheck-mode -1)))
+  (add-hook 'irony-mode-hook (lambda () (flycheck-mode -1)))
   )
 
 (define-key irony-mode-map (kbd "C-c C-c") 'recompile)
@@ -1560,7 +1552,7 @@ Version 2016-01-12"
 	(quote
 	 (window-divider-mode nil))))
  '(doom-modeline-mode nil)
- '(fci-rule-color "#d6d6d6")
+ '(fci-rule-color "#d6d6d6" t)
  '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
  '(frame-background-mode (quote light))
  '(helm-completion-style (quote helm))
@@ -1590,7 +1582,7 @@ Version 2016-01-12"
  '(org-agenda-files (quote ("~/org/work.org")))
  '(package-selected-packages
    (quote
-	(oceanic-theme gdscript-mode plan9-theme flycheck-crystal crystal-mode dracula-theme smart-modeline xterm-color ruby-refactor seeing-is-believing quack sly-quicklisp sly geiser psc-ide flycheck-purescript purescript-mode angular-html-mode fsharp-mode racket-mode cider rainbow-delimiters zenburn-theme yaml-mode yafolding xref-js2 writeroom-mode wrap-region window-numbering which-key web-mode vue-mode use-package undo-tree tide sublime-themes spotify spacemacs-theme smex smartparens scss-mode rust-mode ruby-test-mode ruby-end rubocopfmt rspec-mode robe rjsx-mode restart-emacs rbenv pyenv-mode-auto projectile-rails prettier-js poly-R paredit ox-reveal org-bullets omnisharp olivetti neotree naysayer-theme multi-term mocha lush-theme luarocks lsp-vue lsp-treemacs lsp-ruby lsp-haskell lsp-elixir linum-relative key-chord json-mode jedi irony indium htmlize helm-rg helm-projectile helm-ag haml-mode gruvbox-theme graphql-mode goto-last-change go-autocomplete git-gutter-fringe+ general forge flymake-lua flycheck-rust flycheck-haskell flycheck-elm flycheck-elixir fish-mode fiplr expand-region exec-path-from-shell ess enh-ruby-mode emmet-mode elpy elm-mode ein dap-mode d-mode counsel company-racer company-lua company-lsp company-jedi company-go color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized better-defaults beacon base16-theme all-the-icons alchemist ag add-node-modules-path)))
+	(alect-themes oceanic-theme gdscript-mode plan9-theme flycheck-crystal crystal-mode dracula-theme smart-modeline xterm-color ruby-refactor seeing-is-believing quack sly-quicklisp sly geiser psc-ide flycheck-purescript purescript-mode angular-html-mode fsharp-mode racket-mode cider rainbow-delimiters zenburn-theme yaml-mode yafolding xref-js2 writeroom-mode wrap-region window-numbering which-key web-mode vue-mode use-package undo-tree tide sublime-themes spotify spacemacs-theme smex smartparens scss-mode rust-mode ruby-test-mode ruby-end rubocopfmt rspec-mode robe rjsx-mode restart-emacs rbenv pyenv-mode-auto projectile-rails prettier-js poly-R paredit ox-reveal org-bullets omnisharp olivetti neotree naysayer-theme multi-term mocha lush-theme luarocks lsp-vue lsp-treemacs lsp-ruby lsp-haskell lsp-elixir linum-relative key-chord json-mode jedi irony indium htmlize helm-rg helm-projectile helm-ag haml-mode gruvbox-theme graphql-mode goto-last-change go-autocomplete git-gutter-fringe+ general forge flymake-lua flycheck-rust flycheck-haskell flycheck-elm flycheck-elixir fish-mode fiplr expand-region exec-path-from-shell ess enh-ruby-mode emmet-mode elpy elm-mode ein dap-mode d-mode counsel company-racer company-lua company-lsp company-jedi company-go color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized better-defaults beacon base16-theme all-the-icons alchemist ag add-node-modules-path)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(sml/active-background-color "#98ece8")
  '(sml/active-foreground-color "#424242")
