661f8d52cfbcc6ffc1f80feac103f771e43339c8
James A. Sral <jamessral@gmail.com>
2020-06-05 17:14:29 -0400

    Add some stuff for org-agenda mode

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-; Turn off mouse interface early in startup to avoid momentary display
+ Turn off mouse interface early in startup to avoid momentary display
 (menu-bar-mode -1)
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
@@ -218,19 +218,22 @@
    "i" '(:ignore t :which-key "insert")
    "i n" 'xah-insert-random-number
    "j" '(:ingore t :which-key "jump")
+   "j j" 'dumb-jump-back
+   "j j" 'dumb-jump-go
    "j l" 'avy-goto-line
    "j w" 'avy-goto-char-2
    "n" '(:ignore t :which-key "notes")
    "n n" 'jas/insert-note
    "n t" 'jas/insert-todo
+   "o" '(:ignore t :which-key "org")
+   "o p" 'jas/go-to-personal-org-file
+   "o w" 'jas/go-to-work-org-file
    "p" '(:ignore t :which-key "project")
    "p p" 'projectile-switch-project
    "p f" 'projectile-find-file
    "p s" 'projectile-ripgrep
    "s" '(:ignore t :which-key "shell")
    "s s" 'multi-term-dedicated-toggle
-   "t" '(:ingore t :which-key "todo")
-   "t t" #'jas/go-to-todo-file
    "u" '(:ignore t :which-key "UI")
    "u c" 'counsel-load-theme
    "u l" 'load-light
@@ -655,7 +658,7 @@
   (add-hook 'vue-mode-hook #'prettier-js-mode)
   :config
   (setq mmm-submode-decoration-level 0)
-  ;; (add-to-list 'auto-mode-alist '("\\.vue\\'" . vue-mode))
+  (add-to-list 'auto-mode-alist '("\\.vue\\'" . vue-mode))
 )
 
 
@@ -961,18 +964,30 @@
   :ensure t
   :config
   (setq org-agenda-files (list "~/org/work.org"
-                             "~/org/home.org")))
+                             "~/org/personal.org")))
 (use-package org-bullets
   :ensure t)
 (add-hook 'org-mode-hook (lambda ()
                            (org-bullets-mode 1)))
 
-(defun jas/go-to-todo-file ()
-  "Edit my personal todo file."
+
+(defun jas/go-to-file-in-split (filepath)
+  "Open file in split.  FILEPATH is an absolute path to file."
   (interactive)
   (split-window-right)
   (windmove-right)
-  (find-file "~/todo.org"))
+  (find-file filepath))
+
+(defun jas/go-to-work-org-file ()
+  "Edit my work todo file."
+  (interactive)
+  (jas/go-to-file-in-split "~/org/work.org"))
+
+(defun jas/go-to-personal-org-file ()
+  "Edit my personal todo file."
+  (interactive)
+  (jas/go-to-file-in-split "~/org/personal.org"))
+
 
 (use-package htmlize
   :ensure t)
@@ -1150,7 +1165,7 @@ Version 2016-01-12"
 
 (defun load-light ()
   (interactive)
-  (load-theme 'spacemacs-light t))
+  (load-theme 'tsdh-light t))
 
 (defun load-blue ()
   (interactive)
@@ -1217,6 +1232,7 @@ Version 2016-01-12"
 
 ;;; Erlang/Elixir
 (use-package erlang
+  :ensure t
   :init
   (add-to-list 'auto-mode-alist '("\\.P\\'" . erlang-mode))
   (add-to-list 'auto-mode-alist '("\\.E\\'" . erlang-mode))
@@ -1356,7 +1372,7 @@ Version 2016-01-12"
  '(org-agenda-files (quote ("~/todo.org")))
  '(package-selected-packages
    (quote
-	(elixir gotest go-projectile oceanic-theme battle-haxe dumb-jump ripgrep counsel-projectile uuidgen diminish flycheck-crystal crystal-mode dracula-theme smart-modeline xterm-color ruby-refactor seeing-is-believing quack sly-quicklisp sly geiser psc-ide flycheck-purescript purescript-mode angular-html-mode fsharp-mode racket-mode cider rainbow-delimiters zenburn-theme yaml-mode yafolding xref-js2 writeroom-mode wrap-region window-numbering which-key web-mode vue-mode use-package undo-tree tide sublime-themes spotify spacemacs-theme smex smartparens scss-mode rust-mode ruby-test-mode ruby-end rubocopfmt rspec-mode robe rjsx-mode restart-emacs rbenv pyenv-mode-auto projectile-rails prettier-js poly-R paredit ox-reveal org-bullets omnisharp olivetti neotree naysayer-theme multi-term mocha lush-theme luarocks lsp-vue lsp-treemacs lsp-ruby lsp-haskell lsp-elixir linum-relative key-chord json-mode jedi irony indium htmlize helm-rg helm-projectile helm-ag haml-mode gruvbox-theme graphql-mode goto-last-change go-autocomplete git-gutter-fringe+ general forge flymake-lua flycheck-rust flycheck-haskell flycheck-elm flycheck-elixir fish-mode fiplr expand-region exec-path-from-shell ess enh-ruby-mode emmet-mode elpy elm-mode ein dap-mode d-mode counsel company-racer company-lua company-lsp company-jedi company-go color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized better-defaults beacon base16-theme all-the-icons alchemist ag add-node-modules-path)))
+	(erlang elixir gotest go-projectile oceanic-theme battle-haxe dumb-jump ripgrep counsel-projectile uuidgen diminish flycheck-crystal crystal-mode dracula-theme smart-modeline xterm-color ruby-refactor seeing-is-believing quack sly-quicklisp sly geiser psc-ide flycheck-purescript purescript-mode angular-html-mode fsharp-mode racket-mode cider rainbow-delimiters zenburn-theme yaml-mode yafolding xref-js2 writeroom-mode wrap-region window-numbering which-key web-mode vue-mode use-package undo-tree tide sublime-themes spotify spacemacs-theme smex smartparens scss-mode rust-mode ruby-test-mode ruby-end rubocopfmt rspec-mode robe rjsx-mode restart-emacs rbenv pyenv-mode-auto projectile-rails prettier-js poly-R paredit ox-reveal org-bullets omnisharp olivetti neotree naysayer-theme multi-term mocha lush-theme luarocks lsp-vue lsp-treemacs lsp-ruby lsp-haskell lsp-elixir linum-relative key-chord json-mode jedi irony indium htmlize helm-rg helm-projectile helm-ag haml-mode gruvbox-theme graphql-mode goto-last-change go-autocomplete git-gutter-fringe+ general forge flymake-lua flycheck-rust flycheck-haskell flycheck-elm flycheck-elixir fish-mode fiplr expand-region exec-path-from-shell ess enh-ruby-mode emmet-mode elpy elm-mode ein dap-mode d-mode counsel company-racer company-lua company-lsp company-jedi company-go color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized better-defaults beacon base16-theme all-the-icons alchemist ag add-node-modules-path)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(sml/active-background-color "#98ece8")
  '(sml/active-foreground-color "#424242")
