1972fa4736b288c1dc7f8ba9792cecc7c3b90a58
James A. Sral <jamessral@gmail.com>
2022-10-20 08:33:20 -0400

    Fixup

--- init.el
+++ init.el
@@ -559,8 +559,6 @@
   (add-hook 'js2-mode-hook #'js2-refactor-mode)
   ;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
   ;; unbind it.
-  (define-key js-mode-map (kbd "M-.") nil)
-
   (add-hook 'js2-mode-hook (lambda ()
                              (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t))))
 
